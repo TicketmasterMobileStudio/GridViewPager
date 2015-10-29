497b7b28ab85d20fb0799ade8b07000257c0c5a6
Chris Pierick <chris.pierick@ticketmaster.com>
2015-10-29 11:48:51 -0400

    Scrollable views are now supported. Removed Wear support.

--- gridviewpager/src/main/java/com/ticketmaster/mobilestudio/gridviewpager/SupportFragmentGridPagerAdapter.java
+++ gridviewpager/src/main/java/com/ticketmaster/mobilestudio/gridviewpager/SupportFragmentGridPagerAdapter.java
@@ -8,8 +8,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.ticketmaster.mobilestudio.gridviewpager.GridPageOptions.BackgroundListener;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,10 +17,6 @@ public abstract class SupportFragmentGridPagerAdapter extends GridPagerAdapter {
     private final Map<String, Point> mFragmentPositions;
     private final Map<Point, String> mFragmentTags;
     private FragmentTransaction mCurTransaction;
-    private static final BackgroundListener NOOP_BACKGROUND_OBSERVER = new BackgroundListener() {
-        public void notifyBackgroundChanged() {
-        }
-    };
 
     public SupportFragmentGridPagerAdapter(FragmentManager fm) {
         this.mFragmentManager = fm;
@@ -58,10 +52,6 @@ public abstract class SupportFragmentGridPagerAdapter extends GridPagerAdapter {
         Point position = new Point(column, row);
         this.mFragmentTags.put(position, tag);
         this.mFragmentPositions.put(tag, position);
-        if(fragment instanceof GridPageOptions) {
-            GridPageOptions backgroundProvider = (GridPageOptions)fragment;
-            backgroundProvider.setBackgroundListener(new BackgroundObserver(tag));
-        }
 
         return fragment;
     }
@@ -79,10 +69,6 @@ public abstract class SupportFragmentGridPagerAdapter extends GridPagerAdapter {
         }
 
         Fragment fragment = (Fragment)object;
-        if(fragment instanceof GridPageOptions) {
-            ((GridPageOptions)fragment).setBackgroundListener(NOOP_BACKGROUND_OBSERVER);
-        }
-
         this.removeFragment(fragment, this.mCurTransaction);
     }
 
@@ -111,9 +97,6 @@ public abstract class SupportFragmentGridPagerAdapter extends GridPagerAdapter {
         String tag = this.mFragmentTags.get(new Point(column, row));
         Fragment f = this.mFragmentManager.findFragmentByTag(tag);
         Drawable bg = GridPagerAdapter.BACKGROUND_NONE;
-        if(f instanceof GridPageOptions) {
-            bg = ((GridPageOptions)f).getBackground();
-        }
 
         return bg;
     }
@@ -138,21 +121,5 @@ public abstract class SupportFragmentGridPagerAdapter extends GridPagerAdapter {
     public Fragment findExistingFragment(int row, int column) {
         String tag = (String)this.mFragmentTags.get(new Point(column, row));
         return tag != null?this.mFragmentManager.findFragmentByTag(tag):null;
-    }
-
-    private class BackgroundObserver implements BackgroundListener {
-        private final String mTag;
-
-        private BackgroundObserver(String tag) {
-            this.mTag = tag;
-        }
-
-        public void notifyBackgroundChanged() {
-            Point pos = SupportFragmentGridPagerAdapter.this.mFragmentPositions.get(this.mTag);
-            if(pos != null) {
-                SupportFragmentGridPagerAdapter.this.notifyPageBackgroundChanged(pos.y, pos.x);
-            }
-
-        }
     }
 }
